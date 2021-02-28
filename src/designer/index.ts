ad8c0c22ca6326b3cc655194d38095834df7e442
Morgan Dam <0xc14m1z@gmail.com>
2021-02-28 13:13:41 +0100

    Draft designer app serving

--- src/designer/index.ts
+++ src/designer/index.ts
@@ -1,7 +1,16 @@
+import FileSystem from "fs";
+import Path from "path";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const source = "./node_modules/next-radenv/dist/designer/app/index.html";
+const frontend = FileSystem.readFileSync(Path.resolve(source)).toString();
+
 export function designer(request: NextApiRequest, response: NextApiResponse) {
-  response.json({
-    endpoint: "designer",
-  });
+  switch (request.method) {
+    case "GET": {
+      return response.send(frontend);
+    }
+  }
+
+  response.status(405).end();
 }
