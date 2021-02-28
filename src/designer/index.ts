d782da4c7c16180366ad70374dd7099f96d87773
Morgan Dam <0xc14m1z@gmail.com>
2021-02-28 15:39:57 +0100

    Read the designer app frontend file before serving

--- src/designer/index.ts
+++ src/designer/index.ts
@@ -2,12 +2,11 @@ import FileSystem from "fs";
 import Path from "path";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const source = "./node_modules/next-radenv/dist/designer/app/index.html";
-const frontend = FileSystem.readFileSync(Path.resolve(source)).toString();
-
 export function designer(request: NextApiRequest, response: NextApiResponse) {
   switch (request.method) {
     case "GET": {
+      const source = "./node_modules/next-radenv/dist/designer/app.html";
+      const frontend = FileSystem.readFileSync(Path.resolve(source)).toString();
       return response.send(frontend);
     }
   }
